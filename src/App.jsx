5e7eb8b9e5cf7996d14683f45dd987851ee98a74
Byrone Kinsly <Kinslybyrone17@gmail.com>
2023-11-07 18:55:54 +0300

    authentication using api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './Component/Navbar'
 import MainNews from './Component/MainNews'
 import NewsCard from './Component/NewsCard'
