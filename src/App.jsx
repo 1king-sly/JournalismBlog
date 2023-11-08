d54551c556b101654236ab817e5635886c356493
nabiswavalencia <113972833+nabiswavalencia@users.noreply.github.com>
2023-11-08 15:51:54 +0300

    Add Blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Homepage from './Pages/Homepage.jsx'
 import LoginForm from './Pages/Login.jsx'
+import Admin from './Pages/Admin.jsx'
 function App() {
   
 
   return (
     <>
       <Homepage/>
+      <Admin/>
+      
     </>
 
   )
