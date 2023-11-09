1160d0f4e8d693dc82893e38b25a60d77b41bc4e
nabiswavalencia <113972833+nabiswavalencia@users.noreply.github.com>
2023-11-09 10:16:12 +0300

    Admin dashboard

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -4,6 +4,317 @@ function Admin() {
   return (
     <>
     {/* blogdashboard */}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+     {/* image section  */}
+     
+       <div className='grid grid-cols-2 col-span-2 gap-6'>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+           <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+           <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+             
+                traveling
+              
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+           <p className='inline-block  text-2xl font-bold'> 
+           Visit the East
+           </p>
+             
+            
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className='flex items-center justify-between'>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Learn More
+        </button>
+            <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Delete
+        </button>
+            </div>
+          
+          </div>
+        </div>
+      </div>
+      {/* side panel  */}
+      <div>
+      <div className="mt-2 mb-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block mb-1 text-xl bold font-semibold text-gray-400">
+                SEARCH
+              </label>
+              <label class="relative block">
+  <span class="sr-only">Search</span>
+  <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+    <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989q-.975.35-1.96.35q-2.402 0-4.066-1.663q-1.664-1.664-1.664-4.065T5.47 5.436q1.663-1.667 4.064-1.667q2.402 0 4.068 1.664q1.666 1.664 1.666 4.067q0 1.042-.369 2.017q-.37.975-.97 1.668l6.262 6.261l-.707.708ZM9.538 14.23q1.99 0 3.361-1.37q1.37-1.37 1.37-3.361q0-1.99-1.37-3.36q-1.37-1.37-3.36-1.37q-1.99 0-3.361 1.37q-1.37 1.37-1.37 3.36q0 1.99 1.37 3.36q1.37 1.37 3.36 1.37Z"/></svg> </svg>
+  </span>
+  <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search here..." type="text" name="search"/>
+</label>
+            </div>
+    </div>
+      <ul className='mt-5 '>
+        <h5 className='mb-1 text-xl bold font-semibold text-gray-400'>CATEGORIES</h5>
+        <li className='text-gray-400 hover:cursor-pointer hover:underline hover:decoration-blue-400'> News  </li><hr className="w-full my-2 border-gray-300" />
+        <li className='text-gray-400 hover:cursor-pointer hover:underline hover:decoration-blue-400'>Sports</li><hr className="w-full my-2 border-gray-300" />
+        <li className='text-gray-400 hover:cursor-pointer hover:underline hover:decoration-blue-400'>Business</li><hr className="w-full my-2 border-gray-300" />
+        <li className='text-gray-400 hover:cursor-pointer hover:underline hover:decoration-blue-400'>Entertaiment</li> <hr className="w-full my-2 border-gray-300" />
+
+      </ul>
+      <ul className='mt-5'>
+        <h5 className='mb-1 text-xl bold font-semibold text-gray-400'>LATEST POST</h5>
+        <div className="flex flex-col justify-center w-9/12">
+          <div className="flex">
+            <div className="mr-4 ">
+              <div className="flex items-center justify-center  mb-3 rounded-full bg-indigo-50">
+              <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+              </div>
+            </div>
+            <div className=''>
+              <h6 className="mb-2  w-5/9 font-semibold leading-5">
+                They urge you to put down
+              </h6>
+              <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <span className="text-gray-600"> 28 Dec 2020 -</span>
+
+              traveling
+            </p>
+              <hr className="w-full my-6 border-gray-300" />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="mr-4">
+              <div className="flex items-center justify-center  mb-3 rounded-full bg-indigo-50">
+              <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+              </div>
+            </div>
+            <div>
+              <h6 className="mb-2 font-semibold leading-5">
+                They urge you to put down
+              </h6>
+              <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <span className="text-gray-600"> 28 Dec 2020 -</span>
+
+              traveling
+            </p>
+              <hr className="w-full my-6 border-gray-300" />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="mr-4">
+              <div className="flex items-center justify-center mb-3 rounded-full bg-indigo-50">
+              <img className='w-full' src="/src/images/news.jpeg" alt="" />
+
+              </div>
+            </div>
+            <div>
+              <h6 className="mb-2 font-semibold leading-5">
+                They urge you to put down
+              </h6>
+              <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <span className="text-gray-600"> 28 Dec 2020 -</span>
+
+              traveling
+            </p>
+              <hr className="w-full my-6 border-gray-300" />
+            </div>
+          </div>
+          </div>
+      </ul>
+      </div>
+      </div>
+    </div>
+    
     
     {/* create new blog */}
     <form action="" className='mt-10' >
@@ -105,6 +416,9 @@ function Admin() {
       </div>
       </div>
     </form>
+
+
+
     </>
   )
 }
