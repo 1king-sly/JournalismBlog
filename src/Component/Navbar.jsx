815c6600fcd9af8154d955a60cc8cd9421ca669e
Byrone Kinsly <Kinslybyrone17@gmail.com>
2023-11-09 09:35:22 +0300

    linked the nav_links

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,12 +6,13 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className=' w-[100vw] text-white flex flex-row bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 h-[65px]  justify-around  items-center max-[475px]:justify-between max-[475px]:px-6 fixed top-0 left-0 mb-10  '>
-        <div><Link to='/'><img className='w-20 h-20 object-cover z-10 cursor-pointer' src="/src/images/logo.png" alt="" /></Link></div>
-        <div className='flex gap-10 '><ul className='flex flex-row w-full h-full gap-10 max-[475px]:hidden'>
-            <li><Link to='/News'>News</Link></li>
-            <li><Link to='/Sports'>Sports</Link></li>
-            <li><Link to='/Business'>Business</Link></li>
-            <li><Link to='/Entertainment'>Entertainment</Link></li>
+        <div><Link to='/Homepage'><img className='w-20 h-20 object-cover z-10 cursor-pointer' src="/src/images/logo.png" alt="" /></Link></div>
+        <div className='flex gap-10 '>
+          <ul className='flex flex-row w-full h-full gap-10 max-[475px]:hidden'>
+              <li><Link to='/News'>News</Link></li>
+              <li><Link to='/Sports'>Sports</Link></li>
+              <li><Link to='/Business'>Business</Link></li>
+              <li><Link to='/Entertainment'>Entertainment</Link></li>
             </ul>
             <div className=' min-[475px]:hidden cursor-pointer'>
             <Icon icon="fa:navicon" width="30" height="30" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import Signin from './Pages/Signin.jsx'
-import LoginForm from './Pages/Login.jsx'
-import Homepage from './Pages/Homepage.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
