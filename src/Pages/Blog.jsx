9893b7d68dfd7914e0e7142effab928287a569bc
ClarenceLaria <clarencelaria@gmail.com>
2023-11-13 10:06:35 +0300

    updated design

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,67 +1,77 @@
 import React from 'react'
 import Navbar from '../Component/Navbar';
+import SidePanel from '../Component/SidePanel';
+
 
 function Blog() {
   return (
     <div>
         <Navbar/>
- <div class=" overflow-hidden bg-white px-6 py-24 sm:py-32  lg:px-0">
- <div class="mx-auto max-w-2xl selection:gap-x-8 gap-y-16 lg:mx-auto lg:max-w-none lg:gap-y-10">
-    <div class="lg:row-start-1 lg:mx-auto  lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="">
-          <p class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
-          <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl uppercase">A better workflow</h1>
-          <div class="sm:text-center flex gap-5 items-center">
-            <a href="/" aria-label="Author" class=" mb-1">
-            <img alt="avatar" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" class="object-cover w-10 h-10 rounded-full shadow-sm" />
+ <div className="grid grid-cols-3 overflow-hidden bg-white px-6 py-24 sm:py-32  lg:px-0">
+ <div className="col-span-2 pl-24">
+  
+    <div className=' '>
+    <div className="lg:row-start-1 lg:mx-auto  lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8">
+      <div className="lg:pr-4">
+        <div className="">
+          {/* <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p> */}
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl uppercase">A better workflow</h1>
+          <div className="sm:text-center flex gap-5 items-center">
+            <a href="/" aria-label="Author" className=" mb-1">
+            <img alt="avatar" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" className="object-cover w-10 h-10 rounded-full shadow-sm" />
             </a>
             <div className='flex gap-6'>
-            <p class="text-sm font-medium leading-4 text-gray-600">Laria Clarence</p>
-            <p class="text-sm font-medium leading-4 text-gray-600">Author</p>
-            <p class="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase sm:text-center">
+            <p className="text-sm font-medium leading-4 text-gray-600">Laria Clarence</p>
+            <p className="text-sm font-medium leading-4 text-gray-600">Author</p>
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase sm:text-center">
             20 Nov 2020
             </p>
             </div>
         </div>
-          <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
         </div>
       </div>
     </div>
-    <div class="m-12 mx-40 lg:top-4 lg:overflow-hidden">
-      <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt=""/>
+    <div className="m-12  lg:top-4 lg:overflow-hidden">
+      <img className=" max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt=""/>
     </div>
-    <div class="lg:row-start-2 lg:mx-auto lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="text-base leading-7 text-gray-700">
+
+    <div className="lg:row-start-2 lg:mx-auto lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8">
+      <div className="lg:pr-4">
+        <div className="text-base leading-7 text-gray-700">
+        <p className="mt-6 leading-8 text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nam voluptatibus praesentium veritatis vitae nisi fugiat. Modi laudantium ab ipsam quo dolor sunt corrupti veritatis sapiente numquam quas? Minima, consequuntur. Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+
           <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
-          <ul role="list" class="mt-8 space-y-8 text-gray-600">
-            <li class="flex gap-x-3">
-              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <ul role="list" className="mt-8 space-y-8 text-gray-600">
+            <li className="flex gap-x-3">
+              <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
               </svg>
-              <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+              <span><strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
             </li>
-            <li class="flex gap-x-3">
-              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <li className="flex gap-x-3">
+              <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
               </svg>
-              <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+              <span><strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
             </li>
-            <li class="flex gap-x-3">
-              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <li className="flex gap-x-3">
+              <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                 <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
               </svg>
-              <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+              <span><strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
             </li>
           </ul>
-          <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-          <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-          <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
+          <p className="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+          <p className="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
         </div>
       </div>
     </div>
+  </div>
+  </div>
+  <div className='w-11/12'>
+    <SidePanel/>
   </div>
 </div>
 
