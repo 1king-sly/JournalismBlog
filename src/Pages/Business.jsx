59fd9f3fd7f7426cf99ede23055c616b51504be9
Byrone Kinsly <Kinslybyrone17@gmail.com>
2023-11-13 16:58:46 +0300

    time in local time zone and 24 hour clock system

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -14,7 +14,7 @@ function Business  () {
   }, []);
 
   const formatToLocalTime = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short',hour12: false, timeZone: 'UTC' };
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, };
     return new Date(dateString).toLocaleString(undefined, options);
   };
   
--- src/Pages/Entertainment.jsx
+++ src/Pages/Entertainment.jsx
@@ -13,7 +13,7 @@ function Entertainment  () {
       .catch((error) => console.error('Error fetching news data:', error));
   }, []);
   const formatToLocalTime = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short',hour12: false, timeZone: 'UTC' };
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, };
     return new Date(dateString).toLocaleString(undefined, options);
   };
   
