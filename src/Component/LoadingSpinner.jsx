58b4b08a69aac3fcc06ab0a28e3c5ed816acf46d
David-mwas <dmwas704@gmail.com>
2023-11-14 16:00:37 +0300

    loader size

--- src/Component/LoadingSpinner.jsx
+++ src/Component/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ import Loader from "../../public/Loader.json";
 const LoadingSpinner = () => {
   return (
     <div className="w-screen h-screen items-center justify-center flex">
-      <div className=" w-20 h-20">
+      <div style={{ width: "400px", height: "400px" }}>
         <Lottie animationData={Loader} loop={true} />
       </div>
     </div>
