7e01aa0b6e0936a02680f1c11e0dc61edb47e8dc
Amianie <amianielvis8@gmail.com>
2023-11-14 10:13:24 +0300

    Some changes

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -4,10 +4,7 @@ import Dashboard from '../Component/Dashboard';
 import ProfilePage from '../Component/ProfilePage';
 import AdminSidebar from '../Component/AdminSidebar';
 import CreateBlog from './CreateBlog';
-import Business from '../Pages/Business'
-import Login from '../Pages/Login'
-import Entertainment from '../Pages/Entertainment'
-import News from '../Pages/News'
+
 function Admin() {
   const [selectedItem, setSelectedItem] = useState('Dashboard'); // Default selected item
   const [authenticated, setAuthenticated] = useState(false);
@@ -37,32 +34,26 @@ function Admin() {
       <div className='relative'>
         <AdminNavbar />
       </div>
-      
 
-    <div className='flex  mt-[70px] w-screen '>
-      {/* menu section  */}
+      <div className='flex  mt-[80px] w-screen '>
+        <div className='w-1/6'>
+          <AdminSidebar onItemClick={handleSidebarItemClick} />
+        </div>
 
-      <div className='w-1/6'>
-        <AdminSidebar onItemClick={handleSidebarItemClick}/>
-      </div>
-       
-        
-      {/* blogdashboard */}
-      <div className='showcase w-5/6'>
-        <h5 className='mt-5 text-xl w-full  bold font-semibold text-gray-400 -mb-4  px-10'>{selectedItem}</h5>
-        <div className="">
-            <div className=" px-10 py-5 ">
+        <div className='showcase w-5/6'>
+          <h5 className='mt-2 text-xl w-full  bold font-semibold text-gray-400 -mb-4  px-10'>{selectedItem}</h5>
+          <div className=''>
+            <div className=' px-10 py-5 '>
               {/* Render content based on the selected item */}
               {selectedItem === 'Dashboard' && <Dashboard />}
               {selectedItem === 'Add Blog' && <CreateBlog />}
-              {selectedItem === 'Profile Settings' && <ProfilePage />}
-
+              {selectedItem === 'Profile Settings' && <ProfilePage/> }
             </div>
+          </div>
         </div>
       </div>
     </div>
-       </div>
-  ):(<Login/>)
+  ) : null; // Return null if the user is not authenticated
 }
 
 export default Admin;
